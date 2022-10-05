6e817bd01efa9a85a235a95a7ebe2dbb4e7f6603
helen-tan <helentanmz0909@gmail.com>
2022-10-05 16:24:59 +0800

    login logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,9 +22,32 @@ const createUser = (req, res) => {
 // @route   /api/users/login
 // @access  Public
 const loginUser = (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'Login route'
+    const {username, password} = req.body // get username & pw from body of sent req
+
+    // query database for the user with these login credentials
+    let sql = `select * from users where username='${username}' && password='${password}'`
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(400)
+            throw new Error('Incorrect username or password')
+        } else {
+            if(username === results[0].username && password === results[0].password) {
+                console.log(results);
+                res.status(200).send({
+                    message: 'Login successful',
+                    data: results
+                });
+            } else if (!username || !password) {
+                res.status(400).send({
+                    message: 'Please enter your login credentials',
+                });
+            } else {
+                res.status(400).send({
+                    message: 'Invalid username or password',
+                });
+            }
+        }
     })
 }
 
