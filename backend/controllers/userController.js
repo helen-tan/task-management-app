b31b92023b73a587fc906f4712a669f35ddae3de
helen-tan <helentanmz0909@gmail.com>
2022-10-05 13:50:03 +0800

    Error handling with error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,24 @@
+// @desc    Create a user (Register a user)
+// @route   /api/users
+// @access  Public
 const createUser = (req, res) => {
+    const { username, email, password } = req.body
+
+    // validation
+    if (!username || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
+
     res.status(201).json({
         success: true,
         message: 'Create new user route'
     })
 }
 
+// @desc    Login
+// @route   /api/users/login
+// @access  Public
 const loginUser = (req, res) => {
     res.status(200).json({
         success: true,
@@ -12,6 +26,9 @@ const loginUser = (req, res) => {
     })
 }
 
+// @desc    Get all users
+// @route   /api/users
+// @access  Public
 const getAllUsers = (req, res) => {
     res.status(200).json({
         success: true,
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 // Route imports
@@ -7,11 +8,18 @@ const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 
+// Middlewares
+app.use(express.json()) // allow to send raw json
+app.use(express.urlencoded({ extended: false })) // accept urlencoded form
+
 // Routes
 app.get('/', (req, res) => {
     res.status(200).send({message: 'Welcome to the Task Management API'})
 })
-
 app.use('/api/users', userRoutes)
+
+// Error Handler middleware
+app.use(errorHandler)
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT} in ${process.env.NODE_ENV}`))
