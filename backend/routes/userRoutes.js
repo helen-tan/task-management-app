dc3e44a161eb4292114e470fd5a1d6a241342f77
helen-tan <helentanmz0909@gmail.com>
2022-10-05 11:28:06 +0800

    More test routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
+// @desc    Create a user (Register a user)
+// @route   /api/users
+// @access  Public
+router.post('/', (req, res) => {
+    // To be in controller in future
+    res.status(201).json({
+        success: true,
+        message: 'Register route'
+    })
+})
+
+// @desc    Login
+// @route   /api/users/login
+// @access  Public
+router.get('/login', (req, res) => {
+    // To be in controller in future
+    res.status(200).json({
+        success: true,
+        message: 'Login route'
+    })
+})
+
 // @desc    Get all users
 // @route   /api/users
 // @access  Public
@@ -8,7 +30,6 @@ router.get('/', (req, res) => {
     // To be in controller in future
     res.status(200).json({
         success: true,
-        requestMethod: req.requestMethod,
         message: 'This route will display all users'
     })
 })
