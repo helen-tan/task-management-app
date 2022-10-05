26f34e14fc0187e35386844cf9e873ee9eaa5b7e
helen-tan <helentanmz0909@gmail.com>
2022-10-05 09:03:05 +0800

    Test postman connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.get('/', (req, res) => {
+    res.status(200).send({message: 'Welcome to the Task Management API'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT} in ${process.env.NODE_ENV}`))
