5b8883054a6161075cc282f0c10e7c6fc20e3e1c
helen-tan <helentanmz0909@gmail.com>
2022-10-06 10:44:45 +0800

    bcrypt to compare passwords for login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,6 +13,8 @@ const createUser = async (req, res) => {
         throw new Error('Please include all fields')
     }
 
+    // TODO: Find if user aldy exists
+
     // Hash password
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
@@ -41,39 +43,44 @@ const createUser = async (req, res) => {
             console.log(new_user)
         }
     })
+    
 }
 
 // @desc    Login
 // @route   /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
     const {username, password} = req.body // get username & pw from body of sent req
 
     // query database for the user with these login credentials
-    let sql = `select * from users where username='${username}' && password='${password}'`
+    let sql = `select * from users where username='${username}'`
 
-    db.query(sql, (err, results) => {
+    db.query(sql, async (err, results) => {
+        console.log(results)
         if (err) {
             res.status(400)
-            throw new Error('Incorrect username or password')
+            throw new Error('An error occured')
         } else {
-            if (username === results[0].username && password === results[0].password) {
-                console.log(results);
-                res.status(200).send({
-                    success: true,
-                    message: 'Login successful',
-                    data: results
-                });
-            } else if (!username || !password) {
-                res.status(400).send({
-                    success: false,
-                    message: 'Please enter your login credentials',
-                });
+            if (results.length > 0) {
+                const comparison = await bcrypt.compare(password, results[0].password)
+
+                if (comparison) {
+                    res.status(200).send({
+                        success: true,
+                        message: 'Login successful',
+                        data: results
+                    });
+                } else {
+                    res.status(400).send({
+                        success: false,
+                        message: 'Username and password do not match'
+                    })
+                }
             } else {
                 res.status(400).send({
                     success: false,
-                    message: 'Invalid username or password',
-                });
+                    message: 'Username does not exist'
+                })
             }
         }
     })
