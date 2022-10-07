4583851451114789e65d1d88d9b485ce9614683e
helen-tan <helentanmz0909@gmail.com>
2022-10-07 11:19:56 +0800

    Create Group logic

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -5,12 +5,46 @@ const catchAsyncErrors = require('../middleware/catchAsyncErrors')
 // @route   /api/groups
 // @access  Private
 const createGroup = catchAsyncErrors(async (req, res) => {
-    res.status(200).send({
-        success: true,
-        message: 'This connects'
+    const { group_name } = req.body
+
+    // Validation - Check for empty input
+    if (!group_name) {
+        return res.status(400).send({
+            success: false,
+            message: 'Please enter a group name'
+        })
+    }
+
+    // Validation - Regex to validate user input
+    const group_nameRegexp = /^[a-z]{2,}$/   // only alphabets, no numbers, no special chars
+
+    if (!group_name.match(group_nameRegexp)) {
+        res.status(400).send({
+            success: false,
+            message: "Please use only alphabets, with a minimum of 2 characters"
+        })
+    }
+
+    let sql = `insert into groupz (group_name) values ('${group_name}')`
+
+    new_group = { group_name }
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            res.status(400).send({
+                success: false,
+                message: err.code
+            })
+        } else {
+            res.status(201).send({
+                success: true,
+                message: 'Group created successfully',
+                data: new_group,
+            })
+            console.log(new_group)
+        }
     })
 })
-
 
 module.exports = {
     createGroup
