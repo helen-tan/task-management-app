4ea94ab134998238d27fbd6d02c35a494e120254
helen-tan <helentanmz0909@gmail.com>
2022-10-09 14:00:36 +0800

    Update user route and logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -147,7 +147,7 @@ const getAllUsers = (req, res) => {
 }
 
 // @desc    Get current logged-in user data
-// @route   /api/users/me
+// @route   /api/user/me
 // @access  Private
 const getUser = catchAsyncErrors(async (req, res) => {
     // req.username was set from authMiddleware
@@ -166,9 +166,76 @@ const getUser = catchAsyncErrors(async (req, res) => {
     })
 })
 
+// @desc    Update user
+// @route   /api/user/:username
+// @access  Private
+const updateUser = catchAsyncErrors(async (req, res) => {
+    // Get logged in user (from unique username in jwt token)
+    const loggedInUser = req.username
+    // Get user whose info is to be changed (from the params)
+    const username = req.params.username
+    // TODO: User who can update is the admin and the user themselves
+    // TODO: Handle empty or inputs that don't need updating
+
+    // User inputs
+    let { email, password, is_active, groupz } = req.body
+    
+    // Validation - Regex to validate user input
+    const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/    // Valid email string 
+    const passwordRegexp = /^[a-zA-Z0-9\W|_]{8,10}$/                       // alphanumeric with special chars, 8-10 chars
+    const is_activeRegexp = /^([Tt][Rr][Uu][Ee]|[Ff][Aa][Ll][Ss][Ee])$/    // must be 'true' or 'false'
+
+
+    if (!emailRegexp.test(email) || !passwordRegexp.test(password) || !is_activeRegexp.test(is_active)) {
+        return res.status(400).send({
+            success: false,
+            message: 'Please give a valid email, password or account status input'
+        })
+    }
+
+    // Change is_active from num to string
+    is_active = (is_active === "true") ? "1" : "0"
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    let updated_user = {
+        username: username,
+        email : email,
+        password: hashedPassword,
+        is_active : is_active,
+        groupz: groupz
+    }
+
+    // update user
+    db.query('update users set email = ?, password = ?, is_active = ?, groupz = ? where username = ?', [
+        email, 
+        hashedPassword,
+        is_active,
+        groupz,
+        username
+    ], (err, results) => {
+        if (err) {
+            res.status(400).send({
+                success: false,
+                message: err.code
+            })
+        } else {
+            res.status(200).send({
+                success: true,
+                message: 'User updated successfully',
+                data: updated_user,
+            })
+        }
+    })
+    
+})
+
 module.exports = {
     createUser,
     loginUser,
     getAllUsers,
-    getUser
+    getUser,
+    updateUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { createUser, loginUser, getAllUsers, getUser } = require('../controllers/userController')
+const { createUser, loginUser, getAllUsers, getUser, updateUser } = require('../controllers/userController')
 const { protect, authorizeUser } = require('../middleware/authMiddleware')
 
 
@@ -8,5 +8,6 @@ router.post('/', protect, authorizeUser("admin"), createUser)
 router.post('/login', loginUser)
 router.get('/', protect, getAllUsers)
 router.get('/me', protect, getUser)
+router.put('/:username', protect, updateUser)
 
 module.exports = router
