144cfb9f5dcbcd61efc8b40e1867d57509bf9193
helen-tan <helentanmz0909@gmail.com>
2022-10-10 14:25:59 +0800

    Adjusted createGroup route so that only admins can use

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { createGroup, checkGroup, getAllGroups } = require('../controllers/groupController')
-const { protect } = require('../middleware/authMiddleware')
+const { protect, authorizeUser } = require('../middleware/authMiddleware')
 
-router.post('/', protect, createGroup)
+router.post('/', protect, authorizeUser('admin'), createGroup)
 router.get('/', protect, getAllGroups)
 router.post('/checkGroup', protect, checkGroup)
 
