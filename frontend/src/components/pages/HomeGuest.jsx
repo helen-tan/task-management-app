88e8addb07eb4d6d0d628c80f898e931e1dcb14b
helen-tan <helentanmz0909@gmail.com>
2022-10-11 10:56:32 +0800

    Login from frontend with axios attempt 1

--- frontend/src/components/pages/HomeGuest.jsx
+++ frontend/src/components/pages/HomeGuest.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 import Page from '../utils/Page'
+import Axios from 'axios'
 
 function HomeGuest() {
+  const [username, setUsername] = useState()
+  const [password, setPassword] = useState()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    // Axios to send a req to the backend
+    try {
+      const response = await Axios.post('http://localhost:5000/api/users/login', {
+        username,
+        password
+      })
+      if (response.data) {
+        console.log(response.data)
+      } else {
+        console.log('Incorrect username or password')
+      }
+    } catch (err) {
+      console.log("There was an error")
+    }
+  }
+
   return (
     <Page title="Welcome!">
       <section className="flex flex-col justify-center items-center text-3xl mb-5">
@@ -16,28 +38,28 @@ function HomeGuest() {
       </section>
 
       <section className='form'>
-        <form>
+        <form onSubmit={handleSubmit} >
           <div className="form-group">
             <label htmlFor="username-register" className="text-muted mb-1">
               <small>Username</small>
             </label>
-            <input id="username-register" name="username" className="form-control" type="text" placeholder="Enter your username" autoComplete="off" />
+            <input onChange={(e) => setUsername(e.target.value)} id="username-register" name="username" className="form-control" type="text" placeholder="Enter your username" autoComplete="off" />
           </div>
 
           <div className="form-group">
             <label htmlFor="password-register" className="text-muted mb-1">
               <small>Password</small>
             </label>
-            <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+            <input onChange={(e) => setPassword(e.target.value)} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
           </div>
 
           <div className="form-group">
             <button type="submit" className="py-3 mt-4 btn btn-lg btn-active btn-block">
               Login
             </button>
-
           </div>
         </form>
+
       </section>
     </Page>
 
