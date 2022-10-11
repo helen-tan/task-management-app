e2c8208dd8d1d1be2c75e814b4e0275c8e7a5710
helen-tan <helentanmz0909@gmail.com>
2022-10-11 09:52:33 +0800

    Created Home page(for logged in)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 import Header from "./components/shared/Header";
-import Home from "./components/pages/Home";
+import HomeGuest from "./components/pages/HomeGuest";
 import Footer from "./components/shared/Footer";
+import Home from "./components/pages/Home"
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <div className="container">
         <Header />
           <Routes>
-            <Route path='/' element={<Home/> }/>
+            <Route path='/' element={<HomeGuest/> }/>
           </Routes>
         <Footer />
         </div>
--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,45 +1,8 @@
 import React from 'react'
-import { FaSignInAlt } from 'react-icons/fa'
 
 function Home() {
   return (
-    <>
-    <section className="flex flex-col justify-center items-center text-3xl mb-5">
-        <h1 className='font-bold'>
-          Welcome to the Task Management System
-        </h1>
-        <div className='flex items-center text-slate-400 mt-3'>
-          <FaSignInAlt />
-          <p className='ml-3'>Please login to continue</p>
-        </div>
-      </section>
-
-      <section className='form'>
-        <form>
-          <div className="form-group">
-            <label htmlFor="username-register" className="text-muted mb-1">
-              <small>Username</small>
-            </label>
-            <input id="username-register" name="username" className="form-control" type="text" placeholder="Enter your username" autoComplete="off" />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="password-register" className="text-muted mb-1">
-              <small>Password</small>
-            </label>
-            <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
-          </div>
-
-          <div className="form-group">
-            <button type="submit" className="py-3 mt-4 btn btn-lg btn-active btn-block">
-              Login
-            </button>
-
-          </div>
-        </form>
-      </section>
-    </>
-
+    <div>Home</div>
   )
 }
 
