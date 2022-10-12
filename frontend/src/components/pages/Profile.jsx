4d4a5d9bb6501ebe413d4f497f7fc1c2b3ba0f65
helen-tan <helentanmz0909@gmail.com>
2022-10-12 16:40:07 +0800

    Display User profile data

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -2,6 +2,7 @@ import Page from "../utils/Page"
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import BackButton from "../utils/BackButton"
+import { BsPencilSquare } from "react-icons/bs"
 import Axios from "axios"
 
 function Profile() {
@@ -18,9 +19,9 @@ function Profile() {
             const bearer_token = `Bearer ${sessionStorage.getItem('token')}`
             const config = {
                 headers: {
-                  Authorization: bearer_token
+                    Authorization: bearer_token
                 }
-              }
+            }
             try {
                 const response = await Axios.get(`http://localhost:5000/api/users/${params.username}`, config)
                 console.log(response.data)
@@ -30,24 +31,38 @@ function Profile() {
                     email: response.data.data[0].email,
                     groups: response.data.data[0].groupz
                 })
-            } catch(err) {
+            } catch (err) {
                 console.log("There was a problem")
             }
         }
         fetchUserData()
     }, [])
 
-  return (
-    <Page title='Profile'>
-        <div className="flex justify-start mb-5 ml-5">
-            <BackButton url='/dashboard'/>
-        </div>
-        <h2>{profileData.username}</h2>
-        <div>{profileData.email}</div>
-        <div>{profileData.groups}</div>
+    return (
+        <Page title='Profile'>
+            <div className="flex justify-start mb-5 ml-5">
+                <BackButton url='/dashboard' />
+            </div>
 
-    </Page>
-  )
+            <div className="flex flex-col justify-center items-center m-5">
+                <div className="w-3/4">
+                    <div className="flex justify-between items-center">
+                        <h2 className="text-4xl text-left p-4"><strong>{profileData.username}</strong></h2>
+                        <button  className="btn btn-sm gap-2">
+                            <BsPencilSquare/> Edit
+                        </button>
+                    </div>
+                    <hr />
+                    <div className="text-left my-4">
+                        <span className="font-semibold">Email:</span> {profileData.email}
+                    </div>
+                    <div className="text-left my-4">
+                        <span className="font-semibold">Groups:</span> {profileData.groups}
+                    </div>
+                </div>
+            </div>
+        </Page>
+    )
 }
 
 export default Profile
