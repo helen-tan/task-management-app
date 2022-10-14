299be30ea3e2575d080fc40a71e04bc172136d21
helen-tan <helentanmz0909@gmail.com>
2022-10-14 15:18:06 +0800

    temp solution to floating footer

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Page from '../utils/Page'
 
 function Dashboard() {
   return (
     <Page title="Dashboard">
+      <div className="h-screen">
         <h1>Welcome <strong>{sessionStorage.getItem("username")}</strong></h1>
+      </div>
     </Page>
   )
 }
