7fe65892b0496a99f5428e90e88bed22b4a7c483
helen-tan <helentanmz0909@gmail.com>
2022-10-18 17:58:08 +0800

    some navbar styles

--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -8,10 +8,10 @@ function Header(props) {
     <header>
       <div className='flex justify-between items-center py-5 mb-14 border-b-2 border-slate-200'>
         <h4>
-        <Link to='/dashboard' className='px-5'>TMS System</Link>
+          <Link to='/dashboard' className='px-5'>TMS System</Link>
         </h4>
-        {(props.loggedIn) && <HeaderLoggedIn setLoggedIn={props.setLoggedIn} /> }
-      </div>
+        {(props.loggedIn) && <HeaderLoggedIn setLoggedIn={props.setLoggedIn} />}
+      </div> 
     </header>
   )
 }
--- frontend/src/components/shared/HeaderLoggedIn.jsx
+++ frontend/src/components/shared/HeaderLoggedIn.jsx
@@ -11,18 +11,18 @@ function HeaderLoggedIn(props) {
         //setIsAdmin(sessionStorage.getItem("admin")) // CANNOT DO THIS
         // Send request to check if the user is in the group "admin"
         const bearer_token = `Bearer ${sessionStorage.getItem('token')}`
-            const config = {
-                headers: {
-                    Authorization: bearer_token
-                }
+        const config = {
+            headers: {
+                Authorization: bearer_token
             }
+        }
         async function checkIfAdmin() {
             try {
                 const response = await Axios.post(`http://localhost:5000/api/groups/checkGroup`, {
                     username: sessionStorage.getItem("username"),
                     group_name: "admin"
                 }, config)
-                if(response.data) {
+                if (response.data) {
                     // console.log(response.data.inGroup)
                     response.data.inGroup ? setIsAdmin(true) : setIsAdmin(false)
                 }
@@ -39,23 +39,23 @@ function HeaderLoggedIn(props) {
         sessionStorage.clear()
     }
 
-  return (
-    <div className="w-4/12 flex justify-evenly items-center">
+    return (
+        <div className="w-4/12 flex justify-evenly items-center gap-2">
 
-        {(isAdmin === true) && (
-        <Link to='/usermanagement'>
-            User Management
-        </Link>)}
+            {(isAdmin === true) && (
+                <Link to='/usermanagement'>
+                    User Management
+                </Link>)}
 
-        <Link to={`/profile/${sessionStorage.getItem("username")}`}>
-            <strong>{sessionStorage.getItem("username")}</strong>
-        </Link>
-        
-        <button onClick={handleLogout} className="btn btn-sm gap-2">
-            <FaSignOutAlt/> Logout
-        </button>
-    </div>
-  )
+            <Link to={`/profile/${sessionStorage.getItem("username")}`}>
+                <strong>{sessionStorage.getItem("username")}</strong>
+            </Link>
+
+            <button onClick={handleLogout} className="btn btn-sm gap-2">
+                <FaSignOutAlt /> Logout
+            </button>
+        </div>
+    )
 }
 
 export default HeaderLoggedIn
