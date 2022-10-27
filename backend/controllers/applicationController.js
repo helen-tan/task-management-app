d90d4ea90d006724750e417cbdb7be1bd57b05b9
helen-tan <helentanmz0909@gmail.com>
2022-10-27 16:16:49 +0800

    Created application in database

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -5,7 +5,69 @@ const catchAsyncErrors = require('../middleware/catchAsyncErrors')
 // @route   /api/applications
 // @access  Private
 const createApplication = catchAsyncErrors(async(req, res) => {
- res.send("Hello from the createApplication route!")
+    const {
+        app_acronym,
+        app_description,
+        app_rnumber,
+        app_startdate,
+        app_enddate,
+        app_permit_create,
+        app_permit_open,
+        app_permit_todolist,
+        app_permit_doing,
+        app_permit_done
+    } = req.body
+
+    new_application = {
+        app_acronym,
+        app_description,
+        app_rnumber,
+        app_startdate,
+        app_enddate,
+        app_permit_create,
+        app_permit_open,
+        app_permit_todolist,
+        app_permit_doing,
+        app_permit_done
+    }
+
+    db.query(`insert into applications (
+        app_acronym, 
+        app_description, 
+        app_rnumber, 
+        app_startdate, 
+        app_enddate, 
+        app_permit_create, 
+        app_permit_open, 
+        app_permit_todolist, 
+        app_permit_doing, 
+        app_permit_done
+    ) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [
+        app_acronym,
+        app_description,
+        app_rnumber,
+        app_startdate,
+        app_enddate,
+        app_permit_create,
+        app_permit_open,
+        app_permit_todolist,
+        app_permit_doing,
+        app_permit_done
+    ], (err, results) => {
+        if (err) {
+            res.status(400).send({
+                success: false,
+                message: err.code
+            })
+        } else {
+            res.status(201).send({
+                success: true,
+                message: 'Application created successfully',
+                data: new_application,
+            })
+            console.log(new_application)
+        }
+    })
 })
 
 module.exports = {
