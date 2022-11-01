73a92b2d99ec2e2ff948a86dc20dbfccef7ed48f
helen-tan <helentanmz0909@gmail.com>
2022-11-01 17:21:14 +0800

    Edited createPlan to include plan_color

--- backend/controllers/plansController.js
+++ backend/controllers/plansController.js
@@ -5,18 +5,20 @@ const catchAsyncErrors = require('../middleware/catchAsyncErrors')
 // @route   /api/plans
 // @access  Private
 const createPlan = catchAsyncErrors(async(req, res) => {
-    const {
+    let {
         plan_mvp_name,
         plan_startdate,
         plan_enddate,
-        plan_app_acronym
+        plan_app_acronym,
+        plan_color
     } = req.body
+
+    // plan_color to be white (#FFF) if there is no input from the user
+    if (plan_color === "") plan_color = '#FFF'
 
     // Validation: Regex to validate user input
     const plan_mvp_nameRegexp = /^[a-zA-Z0-9_.]{2,20}$/          // only alphanumeric, dots, underscores, no spaces, min 2 mx 20 chars
     const plan_app_acronymRegexp = /^[a-zA-Z0-9]{2,20}$/         // only alphanumeric, no special chars, no spaces, min 2 max 20 chars
-    console.log(plan_mvp_nameRegexp.test(plan_mvp_name))
-    console.log(plan_app_acronym.match(plan_app_acronymRegexp))
 
     if (!plan_app_acronym.match(plan_app_acronymRegexp)) {
         return res.status(200).send({
@@ -35,19 +37,22 @@ const createPlan = catchAsyncErrors(async(req, res) => {
         plan_mvp_name,
         plan_startdate,
         plan_enddate,
-        plan_app_acronym
+        plan_app_acronym,
+        plan_color
     }
 
     db.query(`insert into plans (
         plan_mvp_name,
         plan_startdate,
         plan_enddate,
-        plan_app_acronym
-    ) values (?, ?, ?, ?)`, [
+        plan_app_acronym,
+        plan_color
+    ) values (?, ?, ?, ?, ?)`, [
         plan_mvp_name,
         plan_startdate,
         plan_enddate,
-        plan_app_acronym
+        plan_app_acronym,
+        plan_color
     ], (err, results) => {
         if (err) {
             res.status(400).send({
