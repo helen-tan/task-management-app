bf8bde1ee0a85496de4a16fce2eed5672779bb9c
helen-tan <helentanmz0909@gmail.com>
2022-11-06 11:55:23 +0800

    Added api route for adding notes on task state promote

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { getAllPlansByApp } = require('../controllers/planController')
 const router = express.Router()
-const { createTask, getAllTasksByApp, getOneTask, updateTaskState, updateTaskNotes } = require('../controllers/taskController')
+const { createTask, getAllTasksByApp, getOneTask, updateTaskState, updateTaskNotes, updateTaskStatePromoteNotes } = require('../controllers/taskController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.post('/:app_acronym', protect, createTask)
@@ -9,5 +9,6 @@ router.get('/:app_acronym', protect, getAllTasksByApp)
 router.get('/:task_app_acronym/:task_id', protect, getOneTask)
 router.put('/:task_id/updateState', protect, updateTaskState)
 router.put('/:task_id/updateNotes', protect, updateTaskNotes)
+router.put('/:task_id/updateStateNotes', protect, updateTaskStatePromoteNotes)
 
 module.exports = router
