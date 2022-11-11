0b12c6f4862e493a6ba13e408276349871f039e3
helen-tan <helentanmz0909@gmail.com>
2022-11-11 16:15:09 +0800

    adjusted create app modal height

--- frontend/src/components/shared/AppList.jsx
+++ frontend/src/components/shared/AppList.jsx
@@ -78,7 +78,7 @@ function AppList(props) {
             marginRight: '-50%',
             transform: 'translate(-50%, -50%)',
             width: '80%',
-            height: '90vh',
+            height: 'auto',
             borderRadius: ".5em",
             overflowY: "auto",
         },
