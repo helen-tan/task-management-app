51f064694b189519484eb63aa1cb1b2c2fa38391
helen-tan <helentanmz0909@gmail.com>
2022-11-11 15:07:05 +0800

    Refactored Edit App modal layout

--- frontend/src/components/shared/EditApp.jsx
+++ frontend/src/components/shared/EditApp.jsx
@@ -32,7 +32,7 @@ function EditApp(props) {
                 setEditAppNameInput(response.data.data[0].app_acronym)
                 setEditAppStartdateInput(response.data.data[0].app_startdate.split("T")[0]) // Note: Bcos of timezone probs, getting 1 day before
                 setEditAppEnddateInput(response.data.data[0].app_enddate.split("T")[0])     // Note: Bcos of timezone probs, getting 1 day before
-                setEditAppRnumInput(response.data.data[0].app_rnumber)  
+                setEditAppRnumInput(response.data.data[0].app_rnumber)
                 setEditAppDescriptionInput(response.data.data[0].app_description)
                 setEditAppPermitCreate(response.data.data[0].app_permit_create)
                 setEditAppPermitOpen(response.data.data[0].app_permit_open)
@@ -95,129 +95,127 @@ function EditApp(props) {
                 <h2 className="font-bold text-xl">Edit Application</h2>
                 <button onClick={props.closeEditAppModal}><strong>X</strong></button>
             </div>
-           
+
             <form onSubmit={handleAppEditSubmit}>
                 <div className="form-group">
                     {/*App Name */}
                     <div className="flex gap-2 mb-6 ml-1 mt-1">
-                        <div htmlFor="edit-app-name" className="font-semibold">Application Name:</div>
-                        <div>{editAppNameInput}</div>
+                        <div htmlFor="edit-app-name" className="font-semibold text-sm">Application Name:</div>
+                        <div className="text-sm">{editAppNameInput}</div>
                     </div>
-                    {/* <input
-                        className="form-control"
-                        onChange={(e) => setEditAppNameInput(e.target.value)}
-                        type="text"
-                        placeholder="Rename the app here"
-                        value={editAppNameInput}
-                        id="edit-app-name"
-                        disabled
-                    /> */}
 
                     {/*R_number */}
                     <div className="flex gap-2 mb-6 ml-1 mt-1">
-                        <div htmlFor="edit-app-rnum" className="font-semibold">R Number:</div>
-                        <div>{editAppRnumInput}</div>
+                        <div htmlFor="edit-app-rnum" className="font-semibold text-sm">R Number:</div>
+                        <div className="text-sm">{editAppRnumInput}</div>
                     </div>
-                    {/* <input
-                        className="form-control"
-                        onChange={(e) => setEditAppRnumInput(e.target.value)}
-                        type="number"
-                        placeholder="Enter a number to identify your app"
-                        value={editAppRnumInput}
-                        id="edit-app-rnum"
-                        disabled
-                    /> */}
 
                     <div className="h-px bg-gray-400 mb-6"></div>
 
-                    {/*Start & End Date input */}
-                    <div className="flex flex-col md:flex-row justify-between gap-1">
-                        <div className="w-full">
-                            <label htmlFor="edit-app-startdate" className="font-semibold">Start Date:</label>
-                            <input
-                                onChange={(e) => setEditAppStartdateInput(e.target.value)}
-                                type="date"
-                                value={editAppStartdateInput}
-                                id="edit-app-startdate"
-                                required
-                            />
+                    <div className="flex gap-5">
+                        <div className="w-1/2">
+                            {/*Start & End Date input */}
+                            <div className="flex flex-col md:flex-row justify-between gap-1">
+                                <div className="w-full">
+                                    <label htmlFor="edit-app-startdate" className="font-semibold text-sm">Start Date:</label>
+                                    <input
+                                        onChange={(e) => setEditAppStartdateInput(e.target.value)}
+                                        type="date"
+                                        className="text-xs" 
+                                        value={editAppStartdateInput}
+                                        id="edit-app-startdate"
+                                        required
+                                    />
+                                </div>
+                                <div className="w-full">
+                                    <label htmlFor="edit-app-enddate" className="font-semibold text-sm">End Date:</label>
+                                    <input
+                                        onChange={(e) => setEditAppEnddateInput(e.target.value)}
+                                        type="date"
+                                        className="text-xs" 
+                                        value={editAppEnddateInput}
+                                        id="edit-app-enddate"
+                                        required
+                                    />
+                                </div>
+                            </div>
+
+                            {/*Description input */}
+                            <label htmlFor="edit-app-description" className="font-semibold text-sm">Description:</label>
+                            <textarea
+                                id="edit-app-description"
+                                cols="30"
+                                rows="5"
+                                className="text-xs" 
+                                placeholder="Say a few words about the application..."
+                                value={editAppDescriptionInput}
+                                onChange={(e) => setEditAppDescriptionInput(e.target.value)}
+                            ></textarea>
                         </div>
-                        <div className="w-full">
-                            <label htmlFor="edit-app-enddate" className="font-semibold">End Date:</label>
-                            <input
-                                onChange={(e) => setEditAppEnddateInput(e.target.value)}
-                                type="date"
-                                value={editAppEnddateInput}
-                                id="edit-app-enddate"
-                                required
-                            />
+
+                        <div className="w-1/2">
+                            {/*App_permit_create */}
+                            <label htmlFor="edit-app-permitcreate" className="font-semibold text-sm">App_permit_Create:</label>
+                            <select id="edit-app-permitcreate" required className="text-xs" value={editAppPermitCreate} onChange={(e) => setEditAppPermitCreate(e.target.value)}>
+                                <option value="" disabled>Choose a group...</option>
+                                {props.groupOptions.map((groupOption) => (
+                                    <option key={groupOption.group_name}>{groupOption.group_name}</option>
+                                ))}
+                            </select>
+
+                            <div className="flex gap-1">
+                                {/*App_permit_open */}
+                                <div className="w-1/2">
+                                    <label htmlFor="edit-app-permitopen" className="font-semibold text-sm">App_permit_Open:</label>
+                                    <select id="edit-app-permitopen" required className="text-xs" value={editAppPermitOpen} onChange={(e) => setEditAppPermitOpen(e.target.value)}>
+                                        <option value="" disabled>Choose a group...</option>
+                                        {props.groupOptions.map((groupOption) => (
+                                            <option key={groupOption.group_name}>{groupOption.group_name}</option>
+                                        ))}
+                                    </select>
+                                </div>
+
+                                {/*App_permit_toDoList */}
+                                <div className="w-1/2">
+                                    <label htmlFor="edit-app-permittodolist" className="font-semibold text-sm">App_permit_toDoList:</label>
+                                    <select id="edit-app-permittodolist" required className="text-xs" value={editAppPermitTodolist} onChange={(e) => setEditAppPermitTodolist(e.target.value)}>
+                                        <option value="" disabled>Choose a group...</option>
+                                        {props.groupOptions.map((groupOption) => (
+                                            <option key={groupOption.group_name}>{groupOption.group_name}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className="flex gap-1">
+                                <div className="w-1/2">
+                                    {/*App_permit_Doing */}
+                                    <label htmlFor="edit-app-permitdoing" className="font-semibold text-sm">App_permit_Doing:</label>
+                                    <select id="edit-app-permitdoing" required className="text-xs" value={editAppPermitDoing} onChange={(e) => setEditAppPermitDoing(e.target.value)}>
+                                        <option value="" disabled>Choose a group...</option>
+                                        {props.groupOptions.map((groupOption) => (
+                                            <option key={groupOption.group_name}>{groupOption.group_name}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                                
+                                <div className="w-1/2">
+                                    {/*App_permit_Done */}
+                                    <label htmlFor="edit-app-permitdone" className="font-semibold text-sm">App_permit_Done:</label>
+                                    <select id="edit-app-permitdone" required className="text-xs" value={editAppPermitDone} onChange={(e) => setEditAppPermitDone(e.target.value)}>
+                                        <option value="" disabled>Choose a group...</option>
+                                        {props.groupOptions.map((groupOption) => (
+                                            <option key={groupOption.group_name}>{groupOption.group_name}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-
-                    {/*Description input */}
-                    <label htmlFor="edit-app-description" className="font-semibold">Description:</label>
-                    <textarea
-                        id="edit-app-description"
-                        cols="30"
-                        rows="5"
-                        placeholder="Say a few words about the application..."
-                        value={editAppDescriptionInput}
-                        onChange={(e) => setEditAppDescriptionInput(e.target.value)}
-                    ></textarea>
-
-
-                    <div className="font-bold text-base mb-5">Groups permitted to:</div>
-
-                    {/*App_permit_create */}
-                    <label htmlFor="edit-app-permitcreate" className="font-semibold">Create Tasks (App_permit_Create):</label>
-                    <select id="edit-app-permitcreate" required value={editAppPermitCreate} onChange={(e) => setEditAppPermitCreate(e.target.value)}>
-                        <option value="" disabled>Choose a group...</option>
-                        {props.groupOptions.map((groupOption) => (
-                            <option key={groupOption.group_name}>{groupOption.group_name}</option>
-                        ))}
-                    </select>
-
-                    {/*App_permit_open */}
-                    <label htmlFor="edit-app-permitopen" className="font-semibold">Shift Tasks to To-do (App_permit_Open):</label>
-                    <select id="edit-app-permitopen" required value={editAppPermitOpen} onChange={(e) => setEditAppPermitOpen(e.target.value)}>
-                        <option value="" disabled>Choose a group...</option>
-                        {props.groupOptions.map((groupOption) => (
-                            <option key={groupOption.group_name}>{groupOption.group_name}</option>
-                        ))}
-                    </select>
-
-                    {/*App_permit_toDoList */}
-                    <label htmlFor="edit-app-permittodolist" className="font-semibold">Shift Tasks to Doing (App_permit_toDoList):</label>
-                    <select id="edit-app-permittodolist" required value={editAppPermitTodolist} onChange={(e) => setEditAppPermitTodolist(e.target.value)}>
-                        <option value="" disabled>Choose a group...</option>
-                        {props.groupOptions.map((groupOption) => (
-                            <option key={groupOption.group_name}>{groupOption.group_name}</option>
-                        ))}
-                    </select>
-
-                    {/*App_permit_Doing */}
-                    <label htmlFor="edit-app-permitdoing" className="font-semibold">Shift Tasks to Done (App_permit_Doing):</label>
-                    <select id="edit-app-permitdoing" required value={editAppPermitDoing} onChange={(e) => setEditAppPermitDoing(e.target.value)}>
-                        <option value="" disabled>Choose a group...</option>
-                        {props.groupOptions.map((groupOption) => (
-                            <option key={groupOption.group_name}>{groupOption.group_name}</option>
-                        ))}
-                    </select>
-
-                    {/*App_permit_Done */}
-                    <label htmlFor="edit-app-permitdone" className="font-semibold">Close Tasks (App_permit_Done):</label>
-                    <select id="edit-app-permitdone" required value={editAppPermitDone} onChange={(e) => setEditAppPermitDone(e.target.value)}>
-                        <option value="" disabled>Choose a group...</option>
-                        {props.groupOptions.map((groupOption) => (
-                            <option key={groupOption.group_name}>{groupOption.group_name}</option>
-                        ))}
-                    </select>
                 </div>
 
-                <button className="btn btn-sm btn-block mt-3" type="submit">Submit</button>
+                <button className="btn btn-sm btn-block" type="submit">Submit</button>
             </form>
-
         </Modal>
     )
 }
